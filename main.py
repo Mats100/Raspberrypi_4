3a5f87e28ae0e8e8e9735efe3acae8ec4f18f528
Mats100 <muneeb75@icloud.com>
2022-08-26 14:30:21 +0500

    Added GPIO 26

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 GPIO.setwarnings(False)
 
 response = None
@@ -13,10 +14,12 @@ try:
     while True:
         if response == "on":
             GPIO.output(21, GPIO.LOW)
+            GPIO.output(26, GPIO.LOW)
             time.sleep(2)
             print("It's ON")
         else:
             GPIO.output(21, GPIO.HIGH)
+            GPIO.output(26, GPIO.HIGH)
             time.sleep(2)
             print("It's OFF")
 
